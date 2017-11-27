16df26ea7bf26e05e81a5da8c136a46c2f60a3a2
Brandon Seah-Dempsey <brandonsd97@gmail.com>
2017-11-27 10:24:21 +0000

    Scrolling background done

---
+++ 3DTetris/Assets/Assets/_Scripts/BGScroller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//
+// C00204076
+// Brandon Seah-Demspey
+// Started at 10:00 27 November 2017
+// Finished at
+// Time taken:
+// Known bugs:
+
+public class BGScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSizeX;
+
+    public Vector3 startPos;
+
+	// Use this for initialization
+	void Start ()
+    {
+        startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, tileSizeX);
+        transform.position = startPos + Vector3.left * newPos;
+	}
+}
