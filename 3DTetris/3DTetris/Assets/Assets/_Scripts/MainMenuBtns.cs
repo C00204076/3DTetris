48c2d8e1a850bab037edbc96cc6c4c71a3b3ed32
Brandon Seah-Dempsey <brandonsd97@gmail.com>
2017-11-30 14:38:10 +0000

    Main Menu UI and scene loading

---
+++ 3DTetris/3DTetris/Assets/Assets/_Scripts/MainMenuBtns.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//
+// C00204076
+// Brandon Seah-Dempsey
+// Started at 14:07 30 November 2017
+// Finished at 14:23 30 November 2017
+// Time taken: 16 Minutes
+// Known bugs: None
+
+
+public class MainMenuBtns : MonoBehaviour
+{
+    //
+    public void PlayGameBtn(string playGameLevel)
+    {
+        SceneManager.LoadScene(playGameLevel);
+    }
+
+    //
+    public void QuitGameBtn()
+    {
+        Application.Quit();
+    }
+}
