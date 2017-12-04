b3eb26ad6dbec9b8c8bb775acdd974233ae49d66
Pierce-James <jamespierce1412@gmail.com>
2017-12-04 12:42:37 +0000

    Added block grid, and Block functions

--- 3DTetris/Assets/Assets/Scripts/InstantiateBlock.cs
+++ 3DTetris/Assets/Assets/Scripts/InstantiateBlock.cs
@@ -3,66 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InstantiateBlock : MonoBehaviour {
-
     public GameObject[] objs;
-    bool isClear = false;
-    Vector3 p = new Vector3(-3, 14, 4.8f);
-    int index = 0;
 
 	/// <summary>
     /// Is called immediately when script is run
     /// </summary>
 	void Start ()
     {
-        isClear = true;
-	}
-	
-	/// <summary>
-    /// Called 60 times per second
-    /// </summary>
-	void Update ()
-    {
-        if (isClear)
-        {
-            Debug.Log("isClear <bool> == true");
-            index = Random.Range(0, 7);
-            Instantiate(objs[index], p, Quaternion.identity);
-            objs[index].transform.Translate(new Vector3(0, -0.1f, 0));
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                Debug.Log("A key pressed");
-                objs[index].transform.Translate(new Vector3(1, 0, 0));
-            }
-            else if (Input.GetKeyDown(KeyCode.D))
-            {
-                Debug.Log("D key pressed");
-                objs[index].transform.Translate(new Vector3(-1, 0, 0));
-            }
-            isClear = false;
-        }
-	}
-
-    /// <summary>
-    /// Detects collision between two GameObjects and 
-    /// sets isClear back to true
-    /// </summary>
-    /// <param name="other"></param>
-    void OnCollisionEnter(Collision other)
-    {
-        Debug.Log("OnCollisionEnter() Called");
-        if (other.gameObject.CompareTag("Boundary"))
-        {
-            Debug.Log(other.gameObject.tag);
-            isClear = true;
-        }
+        SpawnBlock();
     }
 
     /// <summary>
     /// 
     /// </summary>
-    /// <param name="g"></param>
-    void moveDown(GameObject g)
+    public void SpawnBlock()
     {
-        g.transform.Translate(new Vector3(0, -1, 0));
+        int index = Random.Range(0, objs.Length);
+        Instantiate(objs[index], transform.position, Quaternion.identity);
     }
 }
---
+++ 3DTetris/Assets/Assets/Scripts/MoveBlock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBlock : MonoBehaviour {
+
+    bool isAlive = false;
+
+	// Use this for initialization
+	void Start () {
+        isAlive = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (isAlive)
+        {
+            this.transform.Translate(new Vector3(0, -0.1f, 0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+            this.transform.Translate(new Vector3(-1, 0, 0));
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            this.transform.Translate(new Vector3(1, 0, 0));
+        }
+    }
+}
